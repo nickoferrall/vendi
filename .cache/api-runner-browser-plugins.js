7082de961b98c40a25ea5b9ab0ad7a7d4168fede
nickoferrall <nickoferrall@gmail.com>
2019-09-12 21:40:48 +0100

    link..

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/nick/gatsby-link-state-test/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ src/pages/page-2.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = ({ location }) => {
+  // console.log("location....", location.state.testing)
+  return (
+    < Layout >
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      {/* {location.state.testing} */}
+      <Link to="/">Go back to the homepage</Link>
+    </Layout >
+  )
+}
+
+export default SecondPage
