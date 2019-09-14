4c7940c3d2c7ad9fcc71a6a63cc0c8cf2f827795
nickoferrall <nickoferrall@gmail.com>
2019-09-14 22:06:53 +0100

    Added cranky croc hostel as default data

--- src/context/HostelDataContext.js
+++ src/context/HostelDataContext.js
@@ -1,9 +1,12 @@
 import React, { createContext, useState } from "react"
 
-const defaultState = {
-    test: "john"
+const theCrankyCrocHostel = {
+    body: "Located nearby to Bogota most interesting sites, museums & nightlife, this beautifully remodeled colonial house features outdoor patios, lounge with big screen TV, netflix and directv, bar & restaurant, fully equipped guest kitchen, huge TV for sports, WiFi & internet terminals, 24 hour hot water, lockers with electrical sockets & many hooks in the dorms, tourist information, big beds with duvets & reading lamps, towels included, free luggage storage, laundry service & many social events.",
+    city: "Bogota",
+    country: "Colombia",
+    title: "The Cranky Croc Hostel",
 }
-export const HostelDataContext = createContext(defaultState)
+export const HostelDataContext = createContext(theCrankyCrocHostel)
 
 export const HostelDataProvider = ({ children }) => {
     const [clickedHostelData, setClickedHostelData] = useState("");
