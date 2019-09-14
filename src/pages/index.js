a0e49e58b30880b9a82d7a6ceec6ce2fe2bf34f7
nickoferrall <nickoferrall@gmail.com>
2019-09-14 10:55:08 +0100

    Nagivate instead of link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/"
+const IndexPage = () => {
+
+  const handleClick = () => {
+    navigate(
+      "/page-2",
+      {
+        state: { testing: 'ehhsjkdhksjhd' },
+      }
+    )
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      {/* <Link to="/page-2/"
       state={{ testing: 'winning' }}
-    >Go to page 2</Link>
-  </Layout>
-)
+    >Go to page 2</Link> */}
+      <h4 onClick={() => handleClick()}>Page 2</h4>
+    </Layout>
+  )
+}
 
 export default IndexPage
