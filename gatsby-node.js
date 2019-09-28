7f15894e9d9178c3760dce8c093f2284ea1daeb7
nickoferrall <nickoferrall@gmail.com>
2019-09-28 12:28:02 +0100

    Programatically created a page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,42 @@
-// const path = require('path')
-// const { createFilePath } = require('gatsby-source-filesystem')
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
-// const PostTemplate = path.resolve('./src/templates/forSale.js')
+const PostTemplate = path.resolve('./src/templates/forSale.js')
 
-// // exports.onCreateNode = ({ actions, getNode, node }) => {
-// //     const { createNodeField } = actions
-// //     if (node.internal.type === 'MarkdownRemark') {
-// //         const slug = createFilePath({ node, getNode, basePath: 'posts' })
-// //         createNodeField({
-// //             node,
-// //             name: 'slug',
-// //             value: slug
-// //         })
-// //     }
-// // }
-
-// exports.createPages = async ({ actions, graphql }) => {
-//     const { createPage } = actions
-//     await graphql(`
-//     {
-//         allContentfulAllHostels {
-//         edges {
-//             node {
-//                 id
-//                 slug
-//                 }
-//             }
-//         }
-//     }
-//     `)
-//     const posts = result.data.allContentfulAllHostels.edges
-//     posts.forEach(({ node: post }) => {
-//         createPage({
-//             path: `/for-sale/${post.slug}`,
-//             component: PostTemplate,
-//             context: {
-//                 slug: post.slug
-//             }
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//     const { createNodeField } = actions
+//     if (node.internal.type === 'MarkdownRemark') {
+//         const slug = createFilePath({ node, getNode, basePath: 'posts' })
+//         createNodeField({
+//             node,
+//             name: 'slug',
+//             value: slug,
 //         })
-//     })
+//     }
 // }
+
+exports.createPages = async ({ actions, graphql }) => {
+    const { createPage } = actions
+    const result = await graphql(`
+    {
+        allContentfulAllHostels {
+        edges {
+            node {
+                id
+                slug
+                }
+            }
+        }
+    }
+    `)
+    const posts = result.data.allContentfulAllHostels.edges
+    posts.forEach(({ node: post }) => {
+        createPage({
+            path: `/for-sale/${post.slug}`,
+            component: PostTemplate,
+            context: {
+                slug: post.slug
+            }
+        })
+    })
+}
