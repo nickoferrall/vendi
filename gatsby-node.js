b30fb443ad49e8ce61fcf6f5a47a731137cbede2
nickoferrall <nickoferrall@gmail.com>
2019-09-28 11:53:39 +0100

    contenful not working

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// const path = require('path')
+// const { createFilePath } = require('gatsby-source-filesystem')
 
-// You can delete this file if you're not using it
+// const PostTemplate = path.resolve('./src/templates/forSale.js')
+
+// // exports.onCreateNode = ({ actions, getNode, node }) => {
+// //     const { createNodeField } = actions
+// //     if (node.internal.type === 'MarkdownRemark') {
+// //         const slug = createFilePath({ node, getNode, basePath: 'posts' })
+// //         createNodeField({
+// //             node,
+// //             name: 'slug',
+// //             value: slug
+// //         })
+// //     }
+// // }
+
+// exports.createPages = async ({ actions, graphql }) => {
+//     const { createPage } = actions
+//     await graphql(`
+//     {
+//         allContentfulAllHostels {
+//         edges {
+//             node {
+//                 id
+//                 slug
+//                 }
+//             }
+//         }
+//     }
+//     `)
+//     const posts = result.data.allContentfulAllHostels.edges
+//     posts.forEach(({ node: post }) => {
+//         createPage({
+//             path: `/for-sale/${post.slug}`,
+//             component: PostTemplate,
+//             context: {
+//                 slug: post.slug
+//             }
+//         })
+//     })
+// }
---
+++ src/templates/forSale.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const forSale = () => {
+    return (
+        <div>
+            Programatically created!
+        </div>
+    );
+};
+
+export default forSale;
