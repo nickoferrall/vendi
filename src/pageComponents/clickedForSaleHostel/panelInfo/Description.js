fa7fbf1141edc8af7498c9426711680b1475f04e
nickoferrall <nickoferrall@gmail.com>
2019-09-29 17:53:03 +0100

    check if we have data for price

--- src/pageComponents/clickedForSaleHostel/panelInfo/Description.js
+++ src/pageComponents/clickedForSaleHostel/panelInfo/Description.js
@@ -8,24 +8,26 @@ import styles from '../ClickedForSaleHostelStyles.jss'
 import { withStyles } from '@material-ui/core/styles'
 
 const Description = ({ hostelData, classes }) => {
-    return (
-        <Paper className={classes.paperContainer}>
-            <Typography variant="h5">
-                {hostelData.hostelName}
-            </Typography>
-            <Typography variant="h6">
-                ${hostelData.price.toLocaleString()}
-            </Typography>
-            <Typography variant="body2">
-                {hostelData.address}
-            </Typography>
-            <Box className={classes.box}>
-                <Typography variant="body">
-                    {hostelData.description.description}
+    if (hostelData.hostelName) {
+        return (
+            <Paper className={classes.paperContainer}>
+                <Typography variant="h5">
+                    {hostelData.hostelName}
                 </Typography>
-            </Box>
-        </Paper>
-    );
+                <Typography variant="h6">
+                    ${hostelData.price.toLocaleString()}
+                </Typography>
+                <Typography variant="body2">
+                    {hostelData.address}
+                </Typography>
+                <Box className={classes.box}>
+                    <Typography variant="body">
+                        {hostelData.description.description}
+                    </Typography>
+                </Box>
+            </Paper>
+        );
+    }
 };
 
 export default withStyles(styles)(Description)
