cd4c452a2167026801d99f2e32b111542e810f88
nickoferrall <nickoferrall@gmail.com>
2019-10-03 21:19:57 +0100

    New hostels and aligned cards

--- src/pageComponents/homepage/hostelCard/hostelCardStyles.jss.js
+++ src/pageComponents/homepage/hostelCard/hostelCardStyles.jss.js
@@ -1,27 +1,29 @@
-
 const styles = theme => ({
-    card: {
-        height: theme.spacing(57),
-        marginBottom: theme.spacing(9),
-        width: theme.spacing(42),
-        [theme.breakpoints.down('md')]: {
-            marginBottom: theme.spacing(6)
-        }
-    },
-    gridContainer: {
-        padding: theme.spacing(4)
-    },
-    media: {
-        height: theme.spacing(28)
-    },
-    boxTitle: {
-        marginBottom: theme.spacing(4),
-        padding: theme.spacing(2),
-        width: theme.spacing(100)
-    },
-    title: {
-        padding: theme.spacing(2)
+  boxTitle: {
+    marginBottom: theme.spacing(4),
+    padding: theme.spacing(2),
+    width: theme.spacing(100)
+  },
+  card: {
+    marginBottom: theme.spacing(9),
+    padding: 0,
+    width: theme.spacing(42),
+    [theme.breakpoints.down('md')]: {
+      marginBottom: theme.spacing(6)
     }
-})
+  },
+  content: {
+    height: theme.spacing(25)
+  },
+  gridContainer: {
+    padding: theme.spacing(4)
+  },
+  media: {
+    height: theme.spacing(28)
+  },
+  title: {
+    padding: theme.spacing(2)
+  }
+});
 
-export default styles
+export default styles;
--- src/pageComponents/homepage/hostelCard/index.js
+++ src/pageComponents/homepage/hostelCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Img from 'gatsby-image'
-import { navigate } from "gatsby"
+import Img from 'gatsby-image';
+import { navigate } from 'gatsby';
 
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
@@ -11,43 +11,42 @@ import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import styles from './hostelCardStyles.jss'
-import { withStyles } from '@material-ui/core/styles'
+import styles from './hostelCardStyles.jss';
+import { withStyles } from '@material-ui/core/styles';
 
 const HostelCard = ({ classes, hostelData }) => {
-    const description = hostelData.description.description
+  const description = hostelData.description.description;
 
-    const handleClick = () => {
-        navigate(`/${hostelData.slug}`)
-    }
+  const handleClick = () => {
+    navigate(`/${hostelData.slug}`);
+  };
 
-    return (
-        <Grid container justify="center" xs={12} md={4} >
-            <Grid item xs={8.5}>
-                <Card className={classes.card} onClick={() => handleClick()}>
-                    <CardActionArea>
-                        <Img className={classes.media} fluid={hostelData.image[0].fluid} />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                {hostelData.hostelName}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                {description.length < 225 ?
-                                    description :
-                                    `${description.substring(0, 225)}...`}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button color="primary" size="small">
-                            More Info
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid >
-        </Grid >
-    );
-}
+  return (
+    <Grid container justify="center" xs={12} md={4}>
+      <Grid item xs={8.5}>
+        <Card className={classes.card} onClick={() => handleClick()}>
+          <CardActionArea>
+            <Img className={classes.media} fluid={hostelData.image[0].fluid} />
+            <CardContent className={classes.content}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {hostelData.hostelName}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                {description.length < 225
+                  ? description
+                  : `${description.substring(0, 225)}...`}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button color="primary" size="small">
+              More Info
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default withStyles(styles)(HostelCard)
-
+export default withStyles(styles)(HostelCard);
