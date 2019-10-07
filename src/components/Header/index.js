a033773754e1383f19c89d04d7c3847b17adb78f
nickoferrall <nickoferrall@gmail.com>
2019-10-07 21:29:01 +0100

    Merged from prisma branch

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,20 @@
-import React from "react"
+import React from 'react';
 
-import BackgroundImage from 'gatsby-background-image'
-import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from 'gatsby-background-image';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import styles from './headerStyles.jss'
-import { withStyles } from '@material-ui/core/styles'
+import styles from './headerStyles.jss';
+import { withStyles } from '@material-ui/core/styles';
 
-const Header = ({ classes, imageHeight = "300px", imgData, title }) => {
+const Header = ({ classes, imageHeight = '300px', imgData, title }) => {
   const data = useStaticQuery(graphql`
     query {
-      homepageImage: file(relativePath: { eq: "headers/colombia-flag-header.jpg" }) {
+      homepageImage: file(
+        relativePath: { eq: "headers/colombia-flag-header.jpg" }
+      ) {
         childImageSharp {
           fluid(maxWidth: 2000, quality: 90) {
             ...GatsbyImageSharpFluid
@@ -20,20 +22,31 @@ const Header = ({ classes, imageHeight = "300px", imgData, title }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <BackgroundImage
       fluid={imgData ? imgData : data.homepageImage.childImageSharp.fluid}
       style={{ opacity: 0.99 }}
     >
-      <Grid container alignItems="center" justify="center" style={{ height: imageHeight }}>
+      <Grid
+        container
+        alignItems="center"
+        justify="center"
+        style={{ height: imageHeight }}
+      >
         <Grid item xs={4} justify="center" className={classes.headerContainer}>
-          <Typography align="center" className={classes.headlineText} variant="h2">{title}</Typography>
+          <Typography
+            align="center"
+            className={classes.headlineText}
+            variant="h2"
+          >
+            {title}
+          </Typography>
         </Grid>
-      </Grid >
-    </BackgroundImage >
-  )
-}
+      </Grid>
+    </BackgroundImage>
+  );
+};
 
-export default withStyles(styles)(Header)
+export default withStyles(styles)(Header);
