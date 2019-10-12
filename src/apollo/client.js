8cb0bae20938b74d4ee69009c0a306c74f04c07e
nickoferrall <nickoferrall@gmail.com>
2019-10-12 18:26:44 +0100

    Able to sign-up with new heroku app

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
-  uri: 'https://vendi-3ca58c3d3f.herokuapp.com/backend/prod'
+  uri: process.env.GATSBY_HEROKU_URL
 });
--- src/gql/userQueries.js
+++ src/gql/userQueries.js
@@ -3,25 +3,27 @@ import gql from 'graphql-tag';
 const CREATE_USER = gql`
   mutation($name: String!, $email: String!, $password: String!) {
     createUser(data: { name: $name, email: $email, password: $password }) {
-      name
-      email
+      user {
+        name
+        email
+      }
       # token
     }
   }
 `;
 
-// const LOGIN = gql`
-//   mutation($email: String!, $password: String!) {
-//     login(data: { email: $email, password: $password }) {
-//       user {
-//         id
-//         name
-//         email
-//       }
-//       token
-//     }
-//   }
-// `;
+const LOGIN = gql`
+  mutation($email: String!, $password: String!) {
+    login(data: { email: $email, password: $password }) {
+      user {
+        id
+        name
+        email
+      }
+      token
+    }
+  }
+`;
 
 // const GET_MY_INFO = gql`
 //   query {
