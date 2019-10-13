00dd0f655021dc4b11942ae9b13a547c01f2abff
nickoferrall <nickoferrall@gmail.com>
2019-10-13 13:27:03 +0100

    updated  uri

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,5 +1,6 @@
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
-  uri: process.env.GATSBY_HEROKU_URL
+  uri: 'https://arcane-reaches-25218.herokuapp.com/'
 });
+// uri: process.env.GATSBY_HEROKU_URL
