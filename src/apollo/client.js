63daec1c41162b623c72c49bed680467d5b7926d
nickoferrall <nickoferrall@gmail.com>
2019-10-13 13:48:27 +0100

    Aded fetch

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,9 @@
 import ApolloClient from 'apollo-boost';
 
+import fetch from 'isomorphic-fetch';
+
 export const client = new ApolloClient({
+  fetch,
   uri: 'https://arcane-reaches-25218.herokuapp.com/'
 });
 // uri: process.env.GATSBY_HEROKU_URL
--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { ApolloProvider } from 'react-apollo';
 
 import { client } from './client';
