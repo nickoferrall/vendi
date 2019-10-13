b3ea1ec4d0466ce86252440a898954a23c28a4df
nickoferrall <nickoferrall@gmail.com>
2019-10-13 12:04:03 +0100

    remove make styles and signup

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,42 +17,42 @@ import Container from '@material-ui/core/Container';
 import Layout from '../components/layout';
 import { LOGIN } from '../gql/userMutations';
 
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white
-    }
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  errorMessage: {
-    color: '#d32f2f'
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-}));
+// const useStyles = makeStyles(theme => ({
+//   '@global': {
+//     body: {
+//       backgroundColor: theme.palette.common.white
+//     }
+//   },
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center'
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main
+//   },
+//   errorMessage: {
+//     color: '#d32f2f'
+//   },
+//   form: {
+//     width: '100%',
+//     marginTop: theme.spacing(1)
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2)
+//   }
+// }));
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [error, setError] = useState(false);
   const [password, setPassword] = useState('');
 
-  const classes = useStyles();
+  // const classes = useStyles();
 
   const [login, { data }] = useMutation(LOGIN);
 
@@ -84,14 +84,17 @@ const SignIn = () => {
       <Layout />
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
+        {/* <div className={classes.paper}> */}
+        <div>
+          {/* <Avatar className={classes.avatar}> */}
+          <Avatar>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          {/* <form className={classes.form} noValidate onSubmit={handleSubmit}> */}
+          <form noValidate onSubmit={handleSubmit}>
             <TextField
               variant="outlined"
               margin="normal"
@@ -121,16 +124,15 @@ const SignIn = () => {
               label="Remember me"
             />
             {error ? (
-              <Typography className={classes.errorMessage}>
-                Unable to login. Try again.
-              </Typography>
+              // <Typography className={classes.errorMessage}>
+              <Typography>Unable to login. Try again.</Typography>
             ) : null}
             <Button
               type="submit"
               fullWidth
               variant="contained"
               color="primary"
-              className={classes.submit}
+              // className={classes.submit}
             >
               Sign In
             </Button>
