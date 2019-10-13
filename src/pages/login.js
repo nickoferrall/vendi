9125b4aaf03c03b5609c4adcaf05cbe7deb47b89
nickoferrall <nickoferrall@gmail.com>
2019-10-13 12:07:13 +0100

    removed everything from login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,156 +1,164 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { Link, navigate } from 'gatsby';
-import { useMutation } from '@apollo/react-hooks';
-
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
-import Layout from '../components/layout';
-import { LOGIN } from '../gql/userMutations';
-
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const useStyles = makeStyles(theme => ({
-//   '@global': {
-//     body: {
-//       backgroundColor: theme.palette.common.white
-//     }
-//   },
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center'
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main
-//   },
-//   errorMessage: {
-//     color: '#d32f2f'
-//   },
-//   form: {
-//     width: '100%',
-//     marginTop: theme.spacing(1)
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2)
-//   }
-// }));
-
-const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState(false);
-  const [password, setPassword] = useState('');
-
-  // const classes = useStyles();
-
-  const [login, { data }] = useMutation(LOGIN);
-
-  const handleSubmit = async event => {
-    event.preventDefault();
-    try {
-      await login({
-        variables: {
-          email,
-          password
-        }
-      });
-    } catch (err) {
-      if (err) {
-        setError(true);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (data) {
-      localStorage.setItem('jwt', data.login.token);
-      navigate('/');
-    }
-  }, [data]);
-
-  return (
-    <>
-      <Layout />
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        {/* <div className={classes.paper}> */}
-        <div>
-          {/* <Avatar className={classes.avatar}> */}
-          <Avatar>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          {/* <form className={classes.form} noValidate onSubmit={handleSubmit}> */}
-          <form noValidate onSubmit={handleSubmit}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              onChange={event => setEmail(event.target.value)}
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              onChange={event => setPassword(event.target.value)}
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            {error ? (
-              // <Typography className={classes.errorMessage}>
-              <Typography>Unable to login. Try again.</Typography>
-            ) : null}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              // className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container justify="flex-end">
-              {/* <Grid item xs>
-                <Link to="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid> */}
-              <Grid item>
-                <Link to={'/signup'}>{"Don't have an account? Sign Up"}</Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
-    </>
-  );
+const login = () => {
+  return <div>login</div>;
 };
 
-export default SignIn;
+export default login;
+
+// import React, { useEffect, useState } from 'react';
+
+// import { Link, navigate } from 'gatsby';
+// import { useMutation } from '@apollo/react-hooks';
+
+// import Avatar from '@material-ui/core/Avatar';
+// import Button from '@material-ui/core/Button';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import Grid from '@material-ui/core/Grid';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import Typography from '@material-ui/core/Typography';
+// import Container from '@material-ui/core/Container';
+
+// import Layout from '../components/layout';
+// import { LOGIN } from '../gql/userMutations';
+
+// // import { makeStyles } from '@material-ui/core/styles';
+
+// // const useStyles = makeStyles(theme => ({
+// //   '@global': {
+// //     body: {
+// //       backgroundColor: theme.palette.common.white
+// //     }
+// //   },
+// //   paper: {
+// //     marginTop: theme.spacing(8),
+// //     display: 'flex',
+// //     flexDirection: 'column',
+// //     alignItems: 'center'
+// //   },
+// //   avatar: {
+// //     margin: theme.spacing(1),
+// //     backgroundColor: theme.palette.secondary.main
+// //   },
+// //   errorMessage: {
+// //     color: '#d32f2f'
+// //   },
+// //   form: {
+// //     width: '100%',
+// //     marginTop: theme.spacing(1)
+// //   },
+// //   submit: {
+// //     margin: theme.spacing(3, 0, 2)
+// //   }
+// // }));
+
+// const SignIn = () => {
+//   const [email, setEmail] = useState('');
+//   const [error, setError] = useState(false);
+//   const [password, setPassword] = useState('');
+
+//   // const classes = useStyles();
+
+//   const [login, { data }] = useMutation(LOGIN);
+
+//   const handleSubmit = async event => {
+//     event.preventDefault();
+//     try {
+//       await login({
+//         variables: {
+//           email,
+//           password
+//         }
+//       });
+//     } catch (err) {
+//       if (err) {
+//         setError(true);
+//       }
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (data) {
+//       localStorage.setItem('jwt', data.login.token);
+//       navigate('/');
+//     }
+//   }, [data]);
+
+//   return (
+//     <>
+//       <Layout />
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         {/* <div className={classes.paper}> */}
+//         <div>
+//           {/* <Avatar className={classes.avatar}> */}
+//           <Avatar>
+//             <LockOutlinedIcon />
+//           </Avatar>
+//           <Typography component="h1" variant="h5">
+//             Sign in
+//           </Typography>
+//           {/* <form className={classes.form} noValidate onSubmit={handleSubmit}> */}
+//           <form noValidate onSubmit={handleSubmit}>
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               id="email"
+//               label="Email Address"
+//               name="email"
+//               onChange={event => setEmail(event.target.value)}
+//               autoComplete="email"
+//               autoFocus
+//             />
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               name="password"
+//               label="Password"
+//               type="password"
+//               onChange={event => setPassword(event.target.value)}
+//               id="password"
+//               autoComplete="current-password"
+//             />
+//             <FormControlLabel
+//               control={<Checkbox value="remember" color="primary" />}
+//               label="Remember me"
+//             />
+//             {error ? (
+//               // <Typography className={classes.errorMessage}>
+//               <Typography>Unable to login. Try again.</Typography>
+//             ) : null}
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               color="primary"
+//               // className={classes.submit}
+//             >
+//               Sign In
+//             </Button>
+//             <Grid container justify="flex-end">
+//               {/* <Grid item xs>
+//                 <Link to="#" variant="body2">
+//                   Forgot password?
+//                 </Link>
+//               </Grid> */}
+//               <Grid item>
+//                 <Link to={'/signup'}>{"Don't have an account? Sign Up"}</Link>
+//               </Grid>
+//             </Grid>
+//           </form>
+//         </div>
+//       </Container>
+//     </>
+//   );
+// };
+
+// export default SignIn;
