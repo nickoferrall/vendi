5220a9191778e5b2725d219c584c7645436d30ee
nickoferrall <nickoferrall@gmail.com>
2019-10-13 14:13:56 +0100

    Reverted to previous client

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+// import React from 'react';
+
+// import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from 'react-apollo';
+// import { renderToString } from 'react-dom/server';
+// import fetch from 'isomorphic-fetch';
+
+// // import Provider from './src/store/provider'; // This can be the React context API or Redux/Mobx
+
+// // gatsby-ssr is required for build regardless if you plan to support SSR
+// export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+//   const client = new ApolloClient({
+//     fetch,
+//     uri: 'https://arcane-reaches-25218.herokuapp.com/'
+//   });
+
+//   const ConnectedBody = () => (
+//     <ApolloProvider client={client}>
+//       {/* <Provider> */}
+//       {bodyComponent}
+//       {/* </Provider> */}
+//     </ApolloProvider>
+//   );
+
+//   replaceBodyHTMLString(renderToString(<ConnectedBody />));
+// };
