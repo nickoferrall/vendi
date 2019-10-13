48784d6583fe653011de19a5c54e0f287d5e6d2b
nickoferrall <nickoferrall@gmail.com>
2019-10-13 15:53:58 +0100

    removed fetch

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,9 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
-import fetch from 'isomorphic-fetch';
+// import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  fetch,
   uri: 'https://arcane-reaches-25218.herokuapp.com/'
 });
 // uri: process.env.GATSBY_HEROKU_URL
