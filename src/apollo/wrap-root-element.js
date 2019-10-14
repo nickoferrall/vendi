c6d3f72117b40c1494daaa3bc4e36e6d9a524fbb
nickoferrall <nickoferrall@gmail.com>
2019-10-14 19:57:18 +0100

    apollo mutation

--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-// import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+// import { ApolloProvider } from 'react-apollo';
+// import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 
 import { client } from './client';
 
-// export const wrapRootElement = ({ element }) => (
-//   <ApolloProvider client={client}>{element}</ApolloProvider>
-// );
-
 export const wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>
-    <ApolloHooksProvider client={client}>{element}</ApolloHooksProvider>
-  </ApolloProvider>
+  <ApolloProvider client={client}>{element}</ApolloProvider>
 );
+
+// export const wrapRootElement = ({ element }) => (
+//   <ApolloProvider client={client}>
+//     <ApolloHooksProvider client={client}>{element}</ApolloHooksProvider>
+//   </ApolloProvider>
+// );
