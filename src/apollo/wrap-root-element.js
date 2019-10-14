d13e855f87691e8806bb6d80299401f2d9066685
nickoferrall <nickoferrall@gmail.com>
2019-10-14 20:00:43 +0100

    apollo client in same file

--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 // import { ApolloProvider } from 'react-apollo';
 // import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 
-import { client } from './client';
+// import { client } from './client';
+
+const client = new ApolloClient({
+  uri: 'https://arcane-reaches-25218.herokuapp.com/'
+});
 
 export const wrapRootElement = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
